e1fe72d8cb681de9919c3f883863c82945065dc0
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-11-05 14:56:59 +0300

    bot work with many meople

--- number_guess_bot.py
+++ number_guess_bot.py
@@ -11,13 +11,7 @@ dp = Dispatcher()
 
 ATTEMPTS = 5
 
-user = {
-    'in_game': False,
-    'secret_number': None,
-    'attempts': None,
-    'total_games': 0,
-    'wins': 0
-}
+users = {}
 
 
 def get_random_number() -> int:
@@ -29,17 +23,26 @@ async def process_start_command(message: Message):
     await message.answer(
         'Добро пожаловать в игру "Угадай число"!\n'
         'Чтобы прочитать правила игры или посмотреть список доступных\n'
-        'команд - отправьте /help'
+        'команд - отправьте /help\n\n'
+        'Правила игры: \n'
+        'Для старта введите "играть".\n'
+        'Я загадываю число от 1 до 20, а Вам нужно его угадать.\n'
+        f'У Вас есть {ATTEMPTS} попыток.\n\n'
     )
+
+    if message.from_user.id not in users.keys():
+        users[message.from_user.id] = {
+            'in_game': False,
+            'secret_number': None,
+            'attempts': None,
+            'total_games': 0,
+            'wins': 0
+        }
 
 
 @dp.message(Command(commands='help'))
 async def process_help_command(message: Message):
     await message.answer(
-       ' Правила игры: \n'
-        'Для старта игры введите "играть".\n'
-        'Я загадываю число от 1 до 20, а Вам нужно его угадать.\n'
-        f'У Вас есть {ATTEMPTS} попыток.\n\n'
         'Доступные команды: \n'
         '/help - правила игры и список команд,\n'
         '/cancel - выйти из игры, \n'
@@ -51,15 +54,15 @@ async def process_help_command(message: Message):
 @dp.message(Command(commands='stat'))
 async def process_stat_command(message: Message):
     await message.answer(
-        f'Всего игр сыграно: {user["total_games"]}.\n'
-        f'Игр выиграно: {user["wins"]}'
+        f'Всего игр сыграно: {users[message.from_user.id]["total_games"]}.\n'
+        f'Игр выиграно: {users[message.from_user.id]["wins"]}'
     )
 
 
 @dp.message(Command(commands='cancel'))
 async def process_cancel_command(message: Message):
-    if user['in_game']:
-        user['in_game'] = False
+    if users[message.from_user.id]['in_game']:
+        users[message.from_user.id]['in_game'] = False
         await message.answer(
             'Вы вышли из игры. Если хотите сыграть снова - напишите об этом.'
         )
@@ -72,10 +75,10 @@ async def process_cancel_command(message: Message):
 
 @dp.message(F.text.lower().in_(['играть', 'да', 'давай', 'хочу']))
 async def process_agree_answer(message: Message):
-    if not user['in_game']:
-        user['in_game'] = True
-        user['secret_number'] = get_random_number()
-        user['attempts'] = ATTEMPTS
+    if not users[message.from_user.id]['in_game']:
+        users[message.from_user.id]['in_game'] = True
+        users[message.from_user.id]['secret_number'] = get_random_number()
+        users[message.from_user.id]['attempts'] = ATTEMPTS
         await message.answer(
             'Я загадал число от 1 до 20, попробуйте отгадать его!'
         )
@@ -87,7 +90,7 @@ async def process_agree_answer(message: Message):
 
 @dp.message(F.text.lower().in_(["нет", "не хочу", "не буду"]))
 async def process_refusal_answer(message: Message):
-    if not user['in_game']:
+    if not users[message.from_user.id]['in_game']:
         await message.answer(
             'Если захотите поиграть, отправьте слово "играть" мне в чат.'
         )
@@ -99,29 +102,28 @@ async def process_refusal_answer(message: Message):
 
 @dp.message(lambda x: x.text and x.text.isdigit() and 1 <= int(x.text) <= 20)
 async def process_number_answer(message: Message):
-    if user['in_game']:
-        if int(message.text) == user['secret_number']:
-            user['in_game'] = False
-            user['total_games'] += 1
-            user['wins'] += 1
+    if users[message.from_user.id]['in_game']:
+        if int(message.text) == users[message.from_user.id]['secret_number']:
+            users[message.from_user.id]['in_game'] = False
+            users[message.from_user.id]['total_games'] += 1
+            users[message.from_user.id]['wins'] += 1
             await message.answer(
-                f'Вы отгадали! Я загадывал число {user["secret_number"]}!\n'
+                f'Вы отгадали! Я загадывал число {users[message.from_user.id]["secret_number"]}!\n'
                 'Будем играть еще?'
             )
-        elif int(message.text) > user["secret_number"]:
-            user['attempts'] -= 1
+        elif int(message.text) > users[message.from_user.id]["secret_number"]:
+            users[message.from_user.id]['attempts'] -= 1
             await message.answer('Moe число меньше.')
-        elif int(message.text) < user["secret_number"]:
-            user['attempts'] -= 1
+        elif int(message.text) < users[message.from_user.id]["secret_number"]:
+            users[message.from_user.id]['attempts'] -= 1
             await message.answer('Мое число больше.')
 
-
-        if user['attempts'] == 0:
-            user['in_game'] = False
-            user['total_games'] += 1
+        if users[message.from_user.id]['attempts'] == 0:
+            users[message.from_user.id]['in_game'] = False
+            users[message.from_user.id]['total_games'] += 1
             await message.answer(
                 'К сожалению, у Вас больше не осталось попыток. Вы проиграли.\n'
-                f'Я загадал число {user["secret_number"]}. \n'
+                f'Я загадал число {users[message.from_user.id]["secret_number"]}. \n'
                 'Давайте сыграем еще раз?'
             )
     else:
@@ -130,17 +132,18 @@ async def process_number_answer(message: Message):
 
 @dp.message()
 async def process_other_answers(message: Message):
-    if user['in_game']:
-        await message.answer(
-            'Пока мы с Вами играем, присылайте, ' 
-            'пожалуйста, числа в пределах от 1 до 20.'
-        )
+    if message.from_user.id in users.keys():
+        if users[message.from_user.id]['in_game']:
+            await message.answer(
+                'Пока мы с Вами играем, присылайте, ' 
+                'пожалуйста, числа в пределах от 1 до 20.'
+            )
     else:
         await message.answer(
             'Я умею только загадывать числа. '
             'Давайте сыграем в эту игру?\n'
-            'Для помощи введите /start или /help,\n'
-            'или "играть", чтобы начать игру.'
+            'Введите /start, чтобы начать работу с ботом.\n'
+            'Для помощи в работе бота введите /help'
         )
 
 
